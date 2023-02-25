dbdd92731fc2fb9d1703744eb0134743570775a5
Q-Digital <no-reply@q-digital.org>
2023-02-25 12:25:32 +0400

    power collections

---
+++ PowerCollections/PowerCollections.Tests/StackTest.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wintellect.PowerCollections.Tests
+{
+    [TestClass]
+    public class StackTest
+    {
+        [TestMethod]
+        public void PushMethodCanAddValueToLastPlaceIntoStack()
+        {
+            Stack<int> _stack = new Stack<int>(10);
+
+            _stack.Push(1);
+            _stack.Push(2);
+            _stack.Push(3);
+
+            Assert.AreEqual(_stack.Count, 3);
+        }
+
+        [TestMethod]
+        public void TopMethodReturnLastItemFromStack()
+        {
+            Stack<int> _stack = new Stack<int>(10);
+
+            _stack.Push(1);
+            Boolean isTopReturnLastValue = _stack.Top() == 1;
+
+            _stack.Push(2);
+            isTopReturnLastValue = (_stack.Top() == 2) && isTopReturnLastValue;
+
+            Assert.IsTrue(isTopReturnLastValue);
+        }
+
+        [TestMethod]
+        public void PopMethodReturnLastItemFromStackAndRemoveItFromStack()
+        {
+            Stack<int> _stack = new Stack<int>(10);
+
+            _stack.Push(1);
+            _stack.Push(2);
+
+            int LastValue = _stack.Top();
+            int PopedValue = _stack.Pop();
+            Boolean Result = (_stack.Top() != LastValue) && LastValue == PopedValue;
+
+            Assert.IsTrue(Result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException), "Nope, my array is full")]
+        public void PushMethodOutOfRangeException()
+        {
+            Stack<int> _stack = new Stack<int>(2);
+            _stack.Push(1);
+            _stack.Push(2);
+            _stack.Push(3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException), "Nope, my array is empty")]
+        public void TopMethodException()
+        {
+            Stack<int> _stack = new Stack<int>(10);
+            int LastItem = _stack.Top();
+        }
+
+        [TestMethod]
+        public void CapacityEqualsExeptingCapacityAfterCreatingStack()
+        {
+            int AvailableCapacity = 100;
+            Stack<int> _stack = new Stack<int>(AvailableCapacity);
+
+            Assert.AreEqual(_stack.Capacity, AvailableCapacity);
+        }
+
+        [TestMethod]
+        public void CountEqualsExeptingCountAfterPushSomeItems()
+        {
+            int ExpectCount = 32;
+
+            Stack<int> _stack = new Stack<int>(100);
+    
+            for (int i = 0; i < ExpectCount; i++)
+            {
+                _stack.Push(i);
+            }
+
+            Assert.AreEqual(_stack.Count, ExpectCount);
+        }
+
+        [TestMethod]
+        public void PusteExistingArrayIntoStackConstructor()
+        {
+            int[] Result = new int[5];
+            int[] ExpectingArray = new int[] { 1, 2, 3, 4, 5};
+            Stack<int> _stack = new Stack<int>(ExpectingArray);
+
+            for (int i = 4; i >= 0; i--) 
+            {
+                Result[i] = _stack.Pop(); 
+            }
+
+            CollectionAssert.AreEqual(Result, ExpectingArray);
+        }
+
+        [TestMethod]
+        public void TestGetEnumeratorMethod()
+        {
+            int[] Result = new int[5];
+            int[] ExpectingArray = new int[] { 1, 2, 3, 4, 5};
+            Stack<int> _stack = new Stack<int>(ExpectingArray);
+
+            int i = 0;
+            var _enumerator = _stack.GetEnumerator();
+            while(_enumerator.MoveNext())
+            {
+                Result[i++] = (int) _enumerator.Current;
+            }
+
+            CollectionAssert.AreEqual(Result, ExpectingArray);
+        }
+    }
+}
---
+++ PowerCollections/PowerCollections/Stack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wintellect.PowerCollections
+{
+    public class Stack<Type> : IEnumerable
+    {
+        private Type[] Array; // stack
+        public int Capacity = 0; // all available count
+        public int Count = 0; // current count in array
+
+        public Stack(int count)
+        {
+            this.Array = new Type[count];
+            this.Capacity = count;
+        }
+
+        public Stack(Type[] array)
+        {
+            this.Array = new Type[array.Length];
+            this.Array = array;
+            this.Capacity = array.Length;
+            this.Count = array.Length;
+        }
+
+        public Type[] Push(Type item)
+        {
+            if (this.Count == this.Capacity)
+            {
+                throw new IndexOutOfRangeException("Nope, my array is full");
+            }
+
+            this.Array[this.Count++] = item;
+            return this.Array;
+        }
+
+        public Type Top()
+        {
+            if (this.Count <= 0)
+            {
+                throw new InvalidOperationException("Nope, my array is empty");
+            }
+
+            return this.Array[this.Count - 1];
+        }
+
+        public Type Pop()
+        {
+            Type returnValue = this.Top();
+
+            this.Array = this.Array.Where((value, index) => index != this.Count - 1).ToArray();
+            this.Count--;
+
+            return returnValue;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this.Array.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
